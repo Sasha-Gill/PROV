416399e472f5f45ca6d6d9f69b40df51032e1146
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2019-10-03 16:33:49 +0200

    prov

---
+++ Prov_klasser_Sasha_Gill_TE17A/Prov_klasser_Sasha_Gill_TE17A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_klasser_Sasha_Gill_TE17A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book AncientBook = new book();
+
+            Console.WriteLine(AncientBook);
+            Console.ReadLine();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Prov_klasser_Sasha_Gill_TE17A/Prov_klasser_Sasha_Gill_TE17A/book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_klasser_Sasha_Gill_TE17A
+{
+    class book
+    {
+        public int price;
+        private string name;
+        private int rarity;
+        private string category;
+        private int actualValue;
+        private bool cursed = false;
+        private Random generator;
+
+
+
+       public book()
+        {
+            generator = new Random();
+            rarity = generator.Next(100);
+            actualValue = generator.Next(1000);
+
+            
+        }
+     
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Denna bok heter " + name+".");
+            Console.WriteLine("Denna bok är "+rarity+"% sällsynt.");
+            Console.WriteLine("Denna bok tillhör kategorin "+category+".");
+            Console.WriteLine("Denna bok kostar "+price+"kr.");
+            Console.ReadLine();
+        }
+
+        public int Evaluate()
+        {
+            int finalPrice = actualValue * rarity;
+            return finalPrice;
+        }
+
+        public string GetCategory()
+        {
+            return category;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+
+
+
+
+
+
+    }
+}
